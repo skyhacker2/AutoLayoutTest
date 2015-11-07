4700ed6c77a47f4c38642d6d8d73e91ad60dc02e
Eleven Chen <skyhacker@126.com>
2015-11-07 20:15:35 +0800

    Initial Commit

---
+++ AutoLayoutTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutTest
+//
+//  Created by Eleven Chen on 15/11/7.
+//  Copyright © 2015年 Hunuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayoutTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutTest
+//
+//  Created by Eleven Chen on 15/11/7.
+//  Copyright © 2015年 Hunuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
